e844dfd2379ebdf5cb11a2026f275d81ee1ce0b6
Icksan Nugraha <icksannugrahaa@gmail.com>
2020-10-19 21:42:02 +0700

    build

---
+++ android/app/src/main/kotlin/com/sh/comat/comat_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sh.comat.comat_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:comat_apps/ui/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp( MainApp() );
